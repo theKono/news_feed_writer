c28d035a487e39a484814c0f21e93206a4f20787
Yu Liang <yu.liang@thekono.com>
2016-08-11 19:24:03 +0800

    Delete model/mysql

--- model/mysql/sharder.go
+++
@@ -1,9 +0,0 @@
-package mysql
-
-// UnseenAndUnread is the default value for NewsFeed.State.
-const UnseenAndUnread = 0
-
-// Sharder is an interface for sharddable record.
-type Sharder interface {
-	Shard() int
-}
